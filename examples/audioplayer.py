2e1a1572727d596084b14a650aaedf9cf4ffc8fe
Grady O'Connell <flipcoder@gmail.com>
2020-04-18 23:47:14 -0700

    soundfile/libsndfile formats support (optional)

--- examples/audioplayer.py
+++ examples/audioplayer.py
@@ -2,18 +2,23 @@
 import os
 import sys
 import time
+
+try:
+    import openal
+except ImportError:
+    sys.path.append("..")
 from openal.audio import SoundSink, SoundSource
-from openal.loaders import load_wav_file
+from openal.loaders import load_file
+
 
 def run():
-    if len (sys.argv) < 2:
-        print ("Usage: %s wavefile" % os.path.basename(sys.argv[0]))
-        print ("    Using an example wav file...")
+    if len(sys.argv) < 2:
+        print("Usage: %s soundfile" % os.path.basename(sys.argv[0]))
+        print("    Using an example wav file...")
         dirname = os.path.dirname(__file__)
         fname = os.path.join(dirname, "hey.wav")
     else:
         fname = sys.argv[1]
-
 
     sink = SoundSink()
     sink.activate()
@@ -21,14 +26,16 @@ def run():
     source = SoundSource(position=[10, 0, 0])
     source.looping = True
 
-    data = load_wav_file(fname)
+    data = load_file(fname)
     source.queue(data)
 
     sink.play(source)
     while source.position[0] > -10:
-        source.position = [source.position[0] - 1,
-                           source.position[1],
-                           source.position[2]]
+        source.position = [
+            source.position[0] - 1,
+            source.position[1],
+            source.position[2],
+        ]
         sink.update()
         print("playing at %r" % source.position)
         time.sleep(2)
--- openal/loaders/__init__.py
+++ openal/loaders/__init__.py
@@ -2,7 +2,15 @@
 import os
 import sys
 import wave
+import array
+from itertools import chain
 from ..audio import SoundData
+
+try:
+    import soundfile  # optional
+except ImportError:
+    soundfile = None
+import numpy
 
 
 __all__ = ["load_wav_file", "load_file"]
@@ -19,16 +27,35 @@ def load_wav_file(fname):
 
 
 # supported extensions
-_FILEEXTENSIONS = {".wav": load_wav_file}
+# _FILEEXTENSIONS = {
+#     ".wav": load_wav_file,
+#     ".ogg": load_ogg_file
+# }
 
 
 def load_file(fname):
     """Loads an audio file into a SoundData object."""
-    ext = os.path.splitext(fname)[1].lower()
-    funcptr = _FILEEXTENSIONS.get(ext, None)
-    if not funcptr:
-        raise ValueError("unsupported audio file type")
-    return funcptr(fname)
+
+    if soundfile:
+
+        with open(fname, "rb") as f:
+            buf, samplerate = soundfile.read(f, dtype="int16")
+
+        lenbuf = len(buf)
+        if buf.size == 0 or type(buf[0]) == numpy.int16:
+            channels = 1
+        else:  # contains channels
+            channels = len(buf[0])
+            buf = tuple(chain(*buf))
+        buf = array.array("h", buf).tobytes()
+        return SoundData(buf, channels, 16, lenbuf * 2, samplerate)
+
+    else:
+
+        if fname.lower().endswith(".wav"):
+            return load_wav_file(fname)
+        else:
+            raise ValueError("unsupported audio file type")
 
 
 def load_stream(source):
