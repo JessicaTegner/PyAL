c4247ea4b5a0945e1d7454ed04b7cba6f5c1d0ee
Matt Vollrath <matt@endpoint.com>
2020-09-03 12:34:33 -0400

    Fix missing SOFT functions on some platforms
    On OS X, the alcGetStringiSOFT and alcResetDeviceSOFT functions are not
    available.
    
    This change makes the absence of these functions a warning instead of a
    fatal error.
    
    PyAL implementations can be made portable by wrapping use of these
    functions in a try block, e.g.
    
    ```
    alc.alcResetDeviceSOFT(sink.device, None)
    hrtf_buffers = [alc.ALCint(),alc.ALCint*4,alc.ALCint()]
    hrtf_select = hrtf_buffers[1](alc.ALC_HRTF_SOFT,alc.ALC_TRUE,alc.ALC_HRTF_ID_SOFT,alc.ALC_HRTF_DISABLED_SOFT)
    alc.alcResetDeviceSOFT(sink.device, hrtf_select)
    ```
    
    becomes:
    
    ```
    try:
        alc.alcResetDeviceSOFT(sink.device, None)
        hrtf_buffers = [alc.ALCint(),alc.ALCint*4,alc.ALCint()]
        hrtf_select = hrtf_buffers[1](alc.ALC_HRTF_SOFT,alc.ALC_TRUE,alc.ALC_HRTF_ID_SOFT,alc.ALC_HRTF_DISABLED_SOFT)
        alc.alcResetDeviceSOFT(sink.device, hrtf_select)
    except AttributeError:
        print('print a warning here')
    ```

--- openal/alc.py
+++ openal/alc.py
@@ -26,8 +26,7 @@ __all__ = ["ALC_FALSE", "ALC_TRUE", "ALC_INVALID", "ALC_FREQUENCY",
            "alcGetError", "alcIsExtensionPresent", "alcGetProcAddress",
            "alcGetEnumValue", "alcGetString", "alcGetIntegerv",
            "alcCaptureOpenDevice", "alcCaptureCloseDevice", "alcCaptureStart",
-           "alcCaptureStop", "alcCaptureSamples", "alcGetStringiSOFT",
-           "alcResetDeviceSOFT",
+           "alcCaptureStop", "alcCaptureSamples",
            ]
 
 _bind = dll.bind_function
@@ -146,8 +145,14 @@ alcCaptureSamples = _bind("alcCaptureSamples", [ctypes.POINTER(ALCdevice),
                                                 ctypes.POINTER(ALCvoid),
                                                 ALCsizei])
 
-alcGetStringiSOFT = _bind("alcGetStringiSOFT", [ctypes.POINTER(ALCdevice),
-                                                  ctypes.POINTER(ALCenum),
-                                                  ctypes.POINTER(ALCsizei)])
-alcResetDeviceSOFT = _bind("alcResetDeviceSOFT", [ctypes.POINTER(ALCdevice),
-                                                  ctypes.POINTER(ALCint)])
+try:
+    alcGetStringiSOFT = _bind("alcGetStringiSOFT", [ctypes.POINTER(ALCdevice),
+                                                      ctypes.POINTER(ALCenum),
+                                                      ctypes.POINTER(ALCsizei)])
+    alcResetDeviceSOFT = _bind("alcResetDeviceSOFT", [ctypes.POINTER(ALCdevice),
+                                                      ctypes.POINTER(ALCint)])
+except AttributeError:
+    import logging
+    logging.warning('alcGetStringiSOFT and alcResetDeviceSOFT not supported on this platform')
+else:
+    __all__.extend(('alcGetStringiSOFT', 'alcResetDeviceSOFT'))
